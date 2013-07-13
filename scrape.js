4eee60c4c56b545f8bddefb28960910a77e6e207
Hargobind Khalsa <hargobind@quickleft.com>
2013-07-12 18:49:26 -0600

    Refactor scrape to output a single file

--- scrape.js
+++ scrape.js
@@ -1,30 +1,36 @@
+var async = require('async');
 var cheerio = require('cheerio');
 var request = require('request');
 var fs = require('fs');
 var characters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "other" ];
-var url, names;
 
-characters.forEach(function(character) {
-
-  url = 'http://www.crunchbase.com/companies?c=' + character;
+async.concat(characters, function(character, callback) {
+  var url = 'http://www.crunchbase.com/companies?c=' + character;
   request(url, function(err, resp, body){
 
     var $ = cheerio.load(body);
-    names = [];
+    var names = [];
 
     links = $('.col2_table_listing a');
     $(links).each(function(i, link){
       names.push($(link).text());
     });
 
-    fs.writeFile("./data/names/"+character+".json", JSON.stringify(names), function(err) {
-      if(err) {
-        console.log(err);
-      } else {
-        console.log("Results saved to "+character+".json!");
-      }
-    });
+    console.log('Names fetched for: ', character);
 
+    callback(null, names);
   });
+}, function(err, names) {
+  if(err) {
+    console.warn(err);
+    return;
+  }
 
+  fs.writeFile('./data/names.json', JSON.stringify(names), function(err) {
+    if(err) {
+      console.warn(err);
+    } else {
+      console.log('Startup names saved to data/names.json');
+    }
+  });
 });
